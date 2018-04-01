a5acfd8ce1c74862ee5d31ec4664c697e67e3661
Ken <khuynh024@gmail.com>
2018-03-31 21:14:25 -0500

    game done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,65 @@
 
-function stats(){
-    console.log("Wins: " + win);
-    console.log("Losses: " + loss);
-    console.log("Guesses: " + lives);
-    console.log("Wrong Answers: "+ wrongArr);
-}
+// function stats(){
+//     console.log("Wins: " + win);
+//     console.log("Losses: " + loss);
+//     console.log("Guesses: " + lives);
+//     console.log("Guessed Words: "+ guessedArr);
+// }
 
-function scoreBoard(){
+function updateScoreBoard(){
     document.getElementById("wins").innerHTML = "Wins: " + win;
     document.getElementById("loss").innerHTML = "Losses: " + loss;
-    document.getElementById("guess").innerHTML = "Letters you have guessed: " + wrongArr;
+    document.getElementById("guess").innerHTML = "Letters you have guessed: " + guessedArr;
     document.getElementById("lives").innerHTML = "Guesses Left: " + lives;
 }
 
-function reset(){
-    var compChoice = wordBox[Math.floor(Math.random() * wordBox.length)];
+function rng(){
+    compChoice = wordBox[Math.floor(Math.random() * wordBox.length)];
     console.log("CPU Choice " + compChoice);
 
 }
 
 
-var l = 5
+var l = 10
 var win = 0;
 var loss = 0;
 var lives = l;
 
-var wrongArr = [];
+var guessedArr = [];
 
 wordBox = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
 
 var compChoice = wordBox[Math.floor(Math.random() * wordBox.length)];
 console.log("CPU Choice " + compChoice);
 
-
 document.onkeyup = function(event){
-
     var userChoice = event.key.toLowerCase();
-    console.log("User Choice " + userChoice);
-    console.log("**********************");
 
-    if (lives > 0){
-            if (userChoice === compChoice) {
-                win++;
+        if (userChoice === compChoice) {
+            win++;
+            lives = l;
+            alert("You win!!! " + "Letter was " + compChoice.toUpperCase())
+            guessedArr = [];
+            updateScoreBoard();
+            rng();
+        }
+
+        else if (userChoice != compChoice) {
+            lives--;
+            guessedArr.push(userChoice);
+            updateScoreBoard();
+
+            if (lives === 0){
+                loss++;
+                guessedArr = [];
                 lives = l;
-                alert("You win!!!")
-                wrongArr = [];
-                scoreBoard();
-                
+                updateScoreBoard();
+                alert("You ran out of guesses! Start over!");
+                rng();
             }
-
-            else if (userChoice != compChoice) {
-                lives--;
-                // alert("WRONG! Guess again!")
-                wrongArr.push(userChoice);
-                scoreBoard();
-
-                 if (lives === 0){
-                     loss++;
-                     wrongArr = [];
-                     lives = l;
-                     scoreBoard();
-                     alert("You ran out of guesses! Start over!");
-
-
-                 }
-
-                }
-            
-            }
-
+    }
     
-    stats();
-
-
 
 }//onkeyup Function
